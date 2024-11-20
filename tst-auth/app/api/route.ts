1e2e07c64a7c96972562b7594f1a83b25f43a6b2
Adli Arindra <adliarindra27@gmail.com>
2024-11-20 08:28:04 +0700

    etyeya

--- tst-auth/app/api/route.ts
+++ tst-auth/app/api/route.ts
@@ -1,5 +1,5 @@
 
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
     return NextResponse.json({
@@ -7,8 +7,9 @@ export async function GET() {
     })
 }
 
-export async function POST(){
+export async function POST(req: NextRequest){
     return NextResponse.json({
-        "msg": "ini API nya make GET"
+        "msg": "ini API nya make POST",
+        "req": req.headers
     })
 }
