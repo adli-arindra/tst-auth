bf49f49d97975b18151e468c0424af91aa1a9274
Adli Arindra <adliarindra27@gmail.com>
2024-11-20 08:20:01 +0700

    ye

--- tst-auth/app/api/route.ts
+++ tst-auth/app/api/route.ts
@@ -1,6 +1,13 @@
-import { NextResponse } from "next/server";
+import { RequestErrorContext } from "next/dist/server/instrumentation/types";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
+    return NextResponse.json({
+        "msg": "ini API nya make GET"
+    })
+}
+
+export async function POST(){
     return NextResponse.json({
         "msg": "ini API nya make GET"
     })
