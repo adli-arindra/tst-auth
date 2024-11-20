7fc19183241625984cec85ecb930ab04f11f3c13
Adli Arindra <adliarindra27@gmail.com>
2024-11-20 08:24:45 +0700

    yeye

--- tst-auth/app/api/route.ts
+++ tst-auth/app/api/route.ts
@@ -1,5 +1,5 @@
-import { RequestErrorContext } from "next/dist/server/instrumentation/types";
-import { NextRequest, NextResponse } from "next/server";
+
+import { NextResponse } from "next/server";
 
 export async function GET() {
     return NextResponse.json({
